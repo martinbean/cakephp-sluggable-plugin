a1256e21c35a927cc0cb62fde4a9c2092a9edbe4
Martin Bean <martin@martinbean.co.uk>
2014-02-01 12:51:54 +0000

    Make sluggable behavior use model's displayField

--- Model/Behavior/SluggableBehavior.php
+++ Model/Behavior/SluggableBehavior.php
@@ -4,21 +4,18 @@ class SluggableBehavior extends ModelBehavior {
     public function setup(Model $Model, $settings = array()) {
         if (!isset($this->settings[$Model->alias])) {
             $this->settings[$Model->alias] = array(
-                'titleColumn' => 'title',
-                'slugColumn' => 'slug',
-                'replacement' => '-'
+                'slugColumn' => 'slug'
             );
         }
         $this->settings[$Model->alias] = array_merge($this->settings[$Model->alias], $settings);
     }
     
     public function beforeSave(Model $Model, $options = array()) {
-        $titleColumn = $this->settings[$Model->alias]['titleColumn'];
+        $displayField = $Model->displayField;
         $slugColumn = $this->settings[$Model->alias]['slugColumn'];
-        $replacement = $this->settings[$Model->alias]['replacement'];
         
-        if (isset($Model->data[$Model->alias][$titleColumn])) {
-            $Model->data[$Model->alias][$slugColumn] = Inflector::slug(strtolower($Model->data[$Model->alias][$titleColumn]), $replacement);
+        if (isset($Model->data[$Model->alias][$displayField])) {
+            $Model->data[$Model->alias][$slugColumn] = Inflector::slug(strtolower($Model->data[$Model->alias][$displayField]), '-');
         }
         return true;
     }
